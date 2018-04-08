bc79beb23adb9955630d14c429fc863a4520f8ba
hundredrab <sourab.jha@kgec.edu.in>
2018-04-08 08:23:11 +0530

    Add single download functionality from CLI

--- utils.py
+++ utils.py
@@ -1,15 +1,26 @@
+import argparse
 import os
 import re
+import youtube_dl
 from mutagen import File
 # import sysargs
 
+ydl_opts = {
+	'format': 'bestaudio/best',
+	'extractaudio' : True,  # only keep the audio
+	'audioformat' : "mp3",  # convert to mp3 
+	'outtmpl' : "music_new/%(title)s-%(id)s.mp3",
+	'noplaylist' : True,    # only download single song, not playlist
+	'quiet' : True, # Don't print anything
+	'no_warnings' : True,
+}
 
 def Wrapper():
     """ 
     The main function, handles all command line arguments etc.
     """
 
-    # TODO: This shoudl properly handle the following, in addition to the flags:
+    # TODO: This should properly handle the following, in addition to the flags:
     # Default: A single song name in the format '{artist} - {song}'.
     # Urls— Billboards and Top Tens at the least.
     # Keywords for billboards genres.
@@ -18,12 +29,33 @@ def Wrapper():
     pass
 
 
-def InitConfig():
+def main():
     """
     Initialize as global variable the settings as found in the config file and the command-line arguments.
     """
-    global DetailsFlag, ExtendedFlag, IgnoreDownloadedFlag, NoDownloadedAddFlag, OutPath, DownloadedListPath
-    pass
+    # global DetailsFlag, ExtendedFlag, IgnoreDownloadedFlag, NoDownloadedAddFlag, OutPath, DownloadedListPath
+    global done_list
+    done_list = []
+    parser = argparse.ArgumentParser()
+    parser.add_argument("titles", type=str, nargs='*',
+                        help="All the names separated by a space will be searched and downloaded.")
+    parser.add_argument("-f", "--file", type=str,
+                        help="Specify the file name, to download using reference as.")
+    parser.add_argument("-u", "--url", type=str,
+                        help="Specify the url where list is located.")
+    parser.add_argument("-r", "--repair-only", action="store_true",
+                        help="Skip downloading and only add metadata to the files.")
+    args = parser.parse_args()
+    if args.url:
+        print("You want an url:", args.url)
+    if args.titles:
+        for t in args.titles:
+            Download(t)
+
+    # if args.file:
+        
+
+
 
 
 def GetMusicFromList(queue):
@@ -50,7 +82,9 @@ def GetMusicFromList(queue):
 
 def GetListFromURL(url):
     """
-    Return a list of strings containing song details after processing the URL. The URL is expected to be a billboards or a top-tens url.
+    Return a list of strings containing song details after processing the URL. 
+
+    The URL is expected to be a billboards or a top-tens url.
     """
     pass
 
@@ -67,7 +101,10 @@ def Download(song):
     """
     Download a song using Youtube-dl and return it's file path.
     """
-    pass
+    if song.strip() not in done_list:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(['ytsearch:' + str(song)])
+        AddToDownloaded(song)
 
 
 def GetBasicDetails(song):
@@ -91,3 +128,6 @@ def AddToDownloaded(song):
     so they won't be downloaded again.
     """
     pass
+
+if __name__=="__main__":
+    main()
